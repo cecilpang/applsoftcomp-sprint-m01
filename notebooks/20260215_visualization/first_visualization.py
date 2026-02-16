f5289eb940c73864942b6d36bdd393f62005ce43
cecilpang <cpang2@binghamton.edu>
2026-02-16 03:41:13 -0500

    visualize again using cleaned data

--- notebooks/20260215_visualization/first_visualization.py
+++ notebooks/20260215_visualization/first_visualization.py
@@ -10,7 +10,15 @@ def _():
     import pandas as pd
     import seaborn as sns
     from pathlib import Path
-    return Path, pd, sns
+    return Path, mo, pd, sns
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # First visualization attempt
+    """)
+    return
 
 
 @app.cell
@@ -29,7 +37,50 @@ def _(Path, pd):
 
 @app.cell
 def _(motality_df, sns):
-    sns.scatterplot(data=motality_df[motality_df.year<=2026], x='gdpcapita', y='motality_rate', hue='year', size='year') #, sizes=(10, 100))
+    sns.scatterplot(data=motality_df, x='gdpcapita', y='motality_rate', hue='year', size='year', sizes=(10, 100))
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # Visualize with cleaned data
+    """)
+    return
+
+
+@app.cell
+def _(Path, motality_df, pd):
+    cleaned_file = Path('./data/preprocessed/cleaned_motality_gdp_df_20260215.csv')
+
+    if cleaned_file.exists():
+        print(f"The file {cleaned_file} exists.")
+        cleaned_file = pd.read_csv(cleaned_file)
+    else:
+        print(f"The file {cleaned_file} does not exist.")
+
+    print(motality_df.info())
+    print(cleaned_file.info())
+    return (cleaned_file,)
+
+
+@app.cell
+def _(cleaned_file, sns):
+    import matplotlib.pyplot as plt
+
+    sns.scatterplot(data=cleaned_file, x='gdpcapita', y='motality_rate', hue='year', size='year', sizes=(10, 100))
+    plt.savefig("paper/figs/scatterplot_motality_gdp.png")
+    plt.show()
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Observations:
+    1. Motality rate decreases with gdp capita increases
+    2. In the old years, before 1900, mortality rates were higher and GDP was lower. Modern years, after 2000, mortality rates are lower, and GDP is higher.
+    """)
     return
 
 
